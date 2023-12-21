d8474573a59c14964d727c5c07cdd59234779577
kabbo25 <kabbo25@gmail.com>
2023-12-21 16:13:22 +0600

    update gitignore for java

--- Codeforces/Codeforces Round 916 (Div. 3)/B_Preparing_for_the_Contest.java
+++ Codeforces/Codeforces Round 916 (Div. 3)/B_Preparing_for_the_Contest.java
@@ -1,34 +1,29 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class B_Preparing_for_the_Contest {
-    public static void main(String[] args) throws IOException {
-        Scanner sc= new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int k=1;k<=t;++k){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            boolean [] arr= new boolean[a+1];
-            ArrayList<Integer> ans= new ArrayList<Integer>();
-            ArrayList<Integer> ans1= new ArrayList<Integer>();
-            arr[Math.max(a-b,1)]=true;
-            ans.add(Math.max(a-b,1));
-            for(int j=1;j<=a;++j){
-                if(arr[j])continue;
-                if(ans.get(0)>j)
-                    ans1.add(j);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            StringBuilder output = new StringBuilder();
+
+            int start = Math.max(a - b, 1);
+            for (int i = start; i <= a; ++i) {
+                output.append(i).append(" ");
             }
-        Collections.sort(ans1,Collections.reverseOrder());
-        ans.addAll(ans1);
-        for(int i=ans.get(0)+1;i<=a;++i)
-            ans.add(i);
-        for(int i=0;i<ans.size();++i){
-            System.out.print(ans.get(i)+" ");
+
+            for (int i = 1; i < start; ++i) {
+                output.insert(0, i + " ");
+            }
+
+            System.out.println(output.toString().trim());
         }
-        System.out.println();
-    }
+
         sc.close();
     }
 }
